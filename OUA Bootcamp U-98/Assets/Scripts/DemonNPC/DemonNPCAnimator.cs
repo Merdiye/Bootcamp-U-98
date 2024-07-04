edb6e34d2c0d5e0b3530caaa0717793717d0a251
Mert <mertuz15@gmail.com>
2024-07-04 18:17:14 +0300

    .

--- OUA Bootcamp U-98/Assets/Scripts/DemonNPC/DemonNPCAnimator.cs
+++ OUA Bootcamp U-98/Assets/Scripts/DemonNPC/DemonNPCAnimator.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class DemonNPC2 : MonoBehaviour
+public class DemonNPCAnimator : MonoBehaviour
 {
 
     public Animator animator;
     [SerializeField] GameObject warrior;
+    public bool isPunchingBool;
 
 
     private void Start()
@@ -17,12 +18,17 @@ public class DemonNPC2 : MonoBehaviour
 
     private void Update()
     {
+        isPunching();
+        isPunchingBool = animator.GetBool("isPunching");
+    }
+
+    public void isPunching()
+    {
         Vector3 distanceToWarrior = transform.position - warrior.transform.position;
-        if(distanceToWarrior.magnitude <= 4.0f)
+        if (distanceToWarrior.magnitude <= 4.0f)
         {
             animator.SetBool("isPunching", true);
         }
-
         else
         {
             animator.SetBool("isPunching", false);
