3677ae6847653fcde8ddb62951820912c96689f6
melikesvs <155529725+melikesvs@users.noreply.github.com>
2024-07-30 00:47:40 +0300

    .

--- OUA Bootcamp U-98/Assets/Scripts/Save&Quit/UIManager.cs
+++ OUA Bootcamp U-98/Assets/Scripts/Save&Quit/UIManager.cs
@@ -9,6 +9,9 @@ public class UIManager : MonoBehaviour
 
     void Start()
     {
+        if (saveButton == null) Debug.LogError("Save button is not assigned!");
+        if (loadButton == null) Debug.LogError("Load button is not assigned!");
+        if (character == null) Debug.LogError("Character is not assigned!");
         saveButton.onClick.AddListener(character.SaveGame);
         loadButton.onClick.AddListener(character.LoadGame);
     }
